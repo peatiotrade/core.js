3850876af3dfc765d8861cd14b4e8f251d77ba6b
xenohunter <xenohunter@yandex.ru>
2017-04-27 19:10:39 +0300

    Added WavesGo and Incent assets

--- distr/wavesplatform-core.js
+++ distr/wavesplatform-core.js
@@ -839,6 +839,24 @@ var Currency = (function () {
         verified: true
     });
 
+    var WGO = new Currency({
+        id: '4eT6R8R2XuTcBuTHiXVQsh2dN2mg3c2Qnp95EWBNHygg',
+        displayName: 'WavesGo',
+        shortName: 'WGO',
+        symbol: 'WGO',
+        precision: 8,
+        verified: true
+    });
+
+    var INC = new Currency({
+        id: 'FLbGXzrpqkvucZqsHDcNxePTkh2ChmEi4GdBfDRRJVof',
+        displayName: 'Incent',
+        shortName: 'INC',
+        symbol: 'INC',
+        precision: 8,
+        verified: true
+    });
+
     function invalidateCache() {
         currencyCache = {};
 
@@ -850,6 +868,8 @@ var Currency = (function () {
         currencyCache[CNY.id] = CNY;
         currencyCache[WCT.id] = WCT;
         currencyCache[MRT.id] = MRT;
+        currencyCache[WGO.id] = WGO;
+        currencyCache[INC.id] = INC;
     }
 
     invalidateCache();
@@ -874,7 +894,9 @@ var Currency = (function () {
         EUR: EUR,
         CNY: CNY,
         WCT: WCT,
-        MRT: MRT
+        MRT: MRT,
+        WGO: WGO,
+        INC: INC
     };
 })();
 
